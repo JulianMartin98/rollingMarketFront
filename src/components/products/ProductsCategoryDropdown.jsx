8fe25d2cb33821ad488bf7abfd02f0b4668fc3d2
Alvaro D. <alvarodeguer@gmail.com>
2024-05-05 17:45:59 -0300

    Agregado componente de un boton para filtrar productos por categoria

---
+++ src/components/products/ProductsCategoryDropdown.jsx
@@ -0,0 +1,38 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import { useContext,useState } from 'react';
+import { ProductsProvider } from '../../context/ProductsContext';
+
+function DropdownCategoria() {
+  const { getCategoria } = useContext(ProductsProvider);
+  
+  const handleCategorySelect = (categoria) => {
+    getCategoria(categoria);
+  };
+
+  return (
+    <Dropdown className='titulo-admin2'>
+      <Dropdown.Toggle  variant="outline-success"  id="dropdown-basic">
+        Buscar por Categoría
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => handleCategorySelect('Bebidas')}>
+          Bebidas
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleCategorySelect('Golosinas')}>
+          Golosinas
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleCategorySelect('Limpieza')}>
+          Limpieza
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleCategorySelect('Lacteos')}>
+          Lácteos
+        </Dropdown.Item>
+        <Dropdown.Item onClick={() => handleCategorySelect('Otros')}>
+          Otros
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default DropdownCategoria;
