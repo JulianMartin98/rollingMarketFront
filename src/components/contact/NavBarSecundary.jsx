75f23efdcbc025215f7e9f17dc2902fdd4abbb0a
Alvaro D. <alvarodeguer@gmail.com>
2024-06-08 17:10:02 -0300

    Arreglada redireccion a la página 404

--- src/components/contact/NavBarSecundary.jsx
+++ src/components/contact/NavBarSecundary.jsx
@@ -6,10 +6,10 @@ export function NavBarSecundary() {
   return (
     <>
       <div className='pieFotoContacto mt-0 mb-4 p-2 bg-body-secondary'>
-        <a className='segundoNavContacto' href="#" variant='nav-underline'>Preguntas Frecuentes</a>
-        <a className='segundoNavContacto' href="#">Sucursales y métodos de entrega</a>
-        <a className='segundoNavContacto' href="#">Medios de Contacto</a>
-        <a className='segundoNavContacto' href="#">Política de Privacidad</a>
+        <a className='segundoNavContacto' href="*" variant='nav-underline'>Preguntas Frecuentes</a>
+        <a className='segundoNavContacto' href="*">Sucursales y métodos de entrega</a>
+        <a className='segundoNavContacto' href="*">Medios de Contacto</a>
+        <a className='segundoNavContacto' href="*">Política de Privacidad</a>
       </div>
     </>
   );
